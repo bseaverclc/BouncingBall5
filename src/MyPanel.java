2375c759d1778b5f0b0cb0a9554c2c83d073f1f9
bseaverclc <bseaver@d155.org>
2024-01-28 14:37:46 +0000

    added a pony image

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -5,12 +5,19 @@ public class MyPanel extends JPanel {
 
     int xLoc = 200;
     int xVel = 2;
+  JLabel label;
+  
 
     Ball myBall;
     public MyPanel(){
 
         setBackground(Color.GREEN);
          myBall = new Ball(200,300);
+      ImageIcon icon = new ImageIcon("Images/ponyT.png");
+      // Scaling the image before putting it on the label
+      icon.setImage(icon.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT));
+      // Creating a Label with an icon on it
+      label = new JLabel(icon);
     }
 
     @Override
@@ -19,6 +26,7 @@ public class MyPanel extends JPanel {
        // System.out.println("Hi");
 //        g.setColor(Color.RED);
 //        g.fillOval(xLoc,300,20,20);
+      add(label);
         myBall.draw(g);
         myBall.move();
 //        xLoc+=xVel;
